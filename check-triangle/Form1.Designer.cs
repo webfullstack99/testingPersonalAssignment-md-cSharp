7b0e4985a6808e6d53fcd59d6cd9d84541f4aac9
webfullstack99 <webfullstack99@gmail.com>
2020-10-24 09:11:39 +0700

    Add project files.

---
+++ check-triangle/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace check_triangle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbA = new System.Windows.Forms.Label();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.lbB = new System.Windows.Forms.Label();
+            this.tbC = new System.Windows.Forms.TextBox();
+            this.lbC = new System.Windows.Forms.Label();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbA
+            // 
+            this.lbA.AutoSize = true;
+            this.lbA.Location = new System.Drawing.Point(270, 85);
+            this.lbA.Name = "lbA";
+            this.lbA.Size = new System.Drawing.Size(52, 17);
+            this.lbA.TabIndex = 0;
+            this.lbA.Text = "Type a";
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(345, 85);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(208, 22);
+            this.tbA.TabIndex = 1;
+            this.tbA.TextChanged += new System.EventHandler(this.tbA_TextChanged);
+            this.tbA.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbA_KeyPress);
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(345, 130);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(208, 22);
+            this.tbB.TabIndex = 3;
+            this.tbB.TextChanged += new System.EventHandler(this.tbB_TextChanged);
+            this.tbB.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbB_KeyPress);
+            // 
+            // lbB
+            // 
+            this.lbB.AutoSize = true;
+            this.lbB.Location = new System.Drawing.Point(270, 130);
+            this.lbB.Name = "lbB";
+            this.lbB.Size = new System.Drawing.Size(52, 17);
+            this.lbB.TabIndex = 2;
+            this.lbB.Text = "Type b";
+            // 
+            // tbC
+            // 
+            this.tbC.Location = new System.Drawing.Point(345, 179);
+            this.tbC.Name = "tbC";
+            this.tbC.Size = new System.Drawing.Size(208, 22);
+            this.tbC.TabIndex = 5;
+            this.tbC.TextChanged += new System.EventHandler(this.tbC_TextChanged);
+            this.tbC.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbC_KeyPress);
+            // 
+            // lbC
+            // 
+            this.lbC.AutoSize = true;
+            this.lbC.Location = new System.Drawing.Point(270, 179);
+            this.lbC.Name = "lbC";
+            this.lbC.Size = new System.Drawing.Size(51, 17);
+            this.lbC.TabIndex = 4;
+            this.lbC.Text = "Type c";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(478, 229);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 6;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.lbResult.Location = new System.Drawing.Point(270, 262);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(0, 31);
+            this.lbResult.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.tbC);
+            this.Controls.Add(this.lbC);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.lbB);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.lbA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbA;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.Label lbB;
+        private System.Windows.Forms.TextBox tbC;
+        private System.Windows.Forms.Label lbC;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Label lbResult;
+    }
+}
+
---
+++ check-triangle/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace check_triangle
+{
+    public partial class Form1 : Form
+    {
+        private String aVal;
+        private String bVal;
+        private String cVal;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // KEY PRESS
+        private void tbA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!isInputValid(tbA, sender, e)) e.Handled = true;
+        }
+
+        private void tbB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!isInputValid(tbB, sender, e)) e.Handled = true;
+        }
+
+        private void tbC_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (!isInputValid(tbC, sender, e)) e.Handled = true;
+        }
+
+        // CHECK INPUT
+        private bool isInputValid(TextBox tb, object sender, KeyPressEventArgs e)
+        {
+            // check is control keys or number
+            if (!isValidLetter(e.KeyChar)) return false;
+
+            // only allow one decimal point
+            if (
+                    (e.KeyChar == '.' || e.KeyChar == ',' ) && 
+                    (
+                        ((sender as TextBox).Text.IndexOf('.') > -1) ||
+                        ((sender as TextBox).Text.IndexOf(',') > -1)
+                    )
+                ) return false;
+
+            if (!char.IsDigit(e.KeyChar))
+            {
+                int position = Convert.ToInt32(tb.SelectionStart.ToString());
+                string tbVal = tb.Text.ToString();
+
+                // operator
+                if (isOperator(e.KeyChar))
+                {
+                    if (position == 0)
+                    {
+                        if (tbVal.Length > 0)
+                        {
+                            if (isOperator(tbVal[0])) return false;
+                        }
+                    }
+                    else
+                    {
+                        if (!isScientificNotation(tbVal[position - 1])) return false;
+                    }
+                }
+
+                // scientific notation
+                else if (isScientificNotation(e.KeyChar))
+                {
+                    if (tbVal.Length > 0)
+                    {
+                        if (char.IsDigit(tbVal[position - 1])) return true;
+                    }
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool isValidLetter(char c)
+        {
+            return (char.IsControl(c) || char.IsDigit(c) || isScientificNotation(c) || isOperator(c) || c == '.' || c == ',');
+        }
+
+        private bool isScientificNotation(char c)
+        {
+            char[] scientificNotationAllowed = { 'e', 'E' };
+            return Array.IndexOf(scientificNotationAllowed, c) > -1;
+        }
+
+        private bool isOperator(char c)
+        {
+            char[] operatorAllowed = { '+', '-' };
+            return Array.IndexOf(operatorAllowed, c) > -1;
+        }
+
+        // ON CHECK
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = Convert.ToDouble(standardizeValue(tbA.Text.ToString()));
+                double b = Convert.ToDouble(standardizeValue(tbB.Text.ToString()));
+                double c = Convert.ToDouble(standardizeValue(tbC.Text.ToString()));
+                checkTriagle(a, b, c);
+
+            }
+            catch (Exception ex) {
+                String errMessage = "Loi chua co du lieu";
+                Console.WriteLine(errMessage);
+                //MessageBox.Show(errMessage);
+                lbResult.Text = errMessage;
+            }
+        }
+
+        // CHECK TRIAGLE
+        private void checkTriagle(double a, double b, double c)
+        {
+            String result = "";
+                Console.WriteLine($"a: {a}");
+                Console.WriteLine($"b: {b}");
+                Console.WriteLine($"c: {c}");
+            if (a+b > c && a+c > b && c+b > a)
+            {
+                if (a==b && b==c && a == c)
+                {
+                    result = "Tam giac deu";
+                }else if (a==c || b==c || a == b)
+                {
+                    result = "Tam giac can";
+
+                }else if (isSquareTriangle(a, b, c))
+                {
+                    result = "Tam giac vuong";
+                }
+                else
+                {
+                    result = "Tam giac thuong";
+                }
+
+            }
+            else 
+            {
+                result = "Khong phai la tam giac";
+            }
+            lbResult.Text = "Ket qua: "+result;
+        }
+
+        private bool isSquareTriangle(double a, double b, double c)
+        {
+            double pA = Math.Pow(a, 2);
+            double pB = Math.Pow(b, 2);
+            double pC = Math.Pow(c, 2);
+            if (pA + pB == pC || pA + pC == pB || pB + pC == pA) return true;
+            return false;
+        }
+
+        private String standardizeValue(String value)
+        {
+            return value.Replace(',', '.');
+        }
+
+        private void tbA_TextChanged(object sender, EventArgs e)
+        {
+            /*
+            TextBox senderTextBox = (sender as TextBox);
+            String value = senderTextBox.Text.ToString();
+            Console.WriteLine("current val: "+ aVal);
+            Console.WriteLine("a paste: "+value);
+
+            // TEST
+            Double n1;
+            bool succeeded1 = Double.TryParse(value, out n1);
+            if (!succeeded1)
+            {
+                // only one , or . or none
+                senderTextBox.Text = aVal;
+                Console.WriteLine("paste failed, old val: "+aVal);
+                senderTextBox.SelectionStart = aVal.Length;
+                senderTextBox.SelectionLength = 0;
+            }
+            else { 
+                 aVal = value;
+                Console.WriteLine("paste success", aVal);
+            }
+            */
+        }
+
+        private void tbB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
